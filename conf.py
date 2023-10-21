9b8d16d35b5d8d92220afc1abd950e579dc20b09
Elina Kamaeva <71763367+elinkamaeva@users.noreply.github.com>
2023-10-21 13:26:21 +0300

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,5 @@
+TOKEN = "YOUR BOT TOKEN"
+WEBHOOK_HOST = 'YOURUSERNAME.pythonanywhere.com'
+WEBHOOK_PORT = '443'
+# телеграм может работать с портами 443, 80, 88 или 8443
+# при этом pythonanywhere разрешает использовать только порт 443 - стандартный https порт
